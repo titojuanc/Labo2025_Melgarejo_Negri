139e52046d0e737db70e9d7c690750fe3b625d96
melganegri <Alumno26.MELGAREJO.Juan@ipm.edu.ar>
2025-04-07 12:35:19 -0300

    Ejercicio 1 a medio hacer.

---
+++ Java/Unidada1/Circulo.java
@@ -0,0 +1,28 @@
+public class Circulo {
+    private float radio;
+    private float area;
+
+
+    public Circulo() {
+        this.radio = 2;
+        this.area = (float) 3.14*(radio*radio);
+    }
+
+    public Circulo(float radio) {
+        this.radio = radio;
+        this.area= 3.14*
+    }
+    public float getRadio(){
+        return radio;
+    }
+    public void setRadio(float radio){
+        this.radio = radio;
+    }
+    public
+
+    public static void main(String[] args) {
+        circulo= new Circulo();
+        circulo2 = new Circulo((float)4.2);
+    }
+}
+
