cbf28c61421b6e38117aaf3935e09a8679dbe145
titojuanc <alumno26.MELGAREJO.Juan@ipm.edu.ar>
2025-04-09 08:06:39 -0300

    Actualización del orden

--- Java/Labo2025_Java/src/Unidad1/Circulo.java
+++ Java/Labo2025_Java/src/Unidad1/Circulo.java
@@ -1,16 +1,12 @@
 public class Circulo {
     private float radio;
-    private float area;
-
 
     public Circulo() {
         this.radio = 2;
-        this.area = (float) 3.14*(radio*radio);
     }
 
     public Circulo(float radio) {
         this.radio = radio;
-        this.area= 3.14*
     }
     public float getRadio(){
         return radio;
@@ -18,11 +14,22 @@ public class Circulo {
     public void setRadio(float radio){
         this.radio = radio;
     }
-    public
+
+    public float getArea(){
+        return (float) 3.14*(this.radio*this.radio);
+    }
+
+    public float getPerimetro(){
+        return (float) 3.14*this.radio*2;
+    }
 
     public static void main(String[] args) {
         circulo= new Circulo();
         circulo2 = new Circulo((float)4.2);
+        circulo.getRadio;
+        circulo2.setRadio(5);
+        circulo.getArea();
+        circulo2.getPerimetro();
     }
 }
 
