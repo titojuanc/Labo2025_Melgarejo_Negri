c690da652c8339d45f784200e5759ee5923d0a0e
Negri <fran.e.negri@gmail.com>
2025-04-22 21:28:46 -0300

    Falta dia en fixture

--- Java/Labo2025_Java/src/Sistema.java
+++ Java/Labo2025_Java/src/Sistema.java
@@ -1,3 +1,5 @@
+import jdk.internal.classfile.BufWriter;
+
 import java.util.ArrayList;
 
 public class Sistema {
@@ -20,20 +22,42 @@ public class Sistema {
         this.equipos = equipos;
     }
 
+    public void ordenarEquipos(ArrayList<Equipo> Pequipos = new ArrayList<Equipo>()){
+        for (int i = 0; i<Pequipos.size(); i+=2){
+            if (i+1<Pequipos.size()){
+                this.fixture.add(Pequipos.get(i),Pequipos.get(i+1));
+            }
+            else {
+                System.out.println("El equipo " + Pequipos.get(i) + " queda afuera por imparidad");
+            }
+        }
+    }
+
     public void generarFixture(){
         ArrayList<Equipo> equiposMañana = new ArrayList<Equipo>();
         ArrayList<Equipo> equiposTarde = new ArrayList<Equipo>();
         ArrayList<Equipo> equiposNoche = new ArrayList<Equipo>();
+
         if(equipos.isEmpty()){
             System.out.println("Falta llenar equipos");
         }
         else{
-            for (int i=0;i<equipos.size();i++){
+            for (int i=0;i<this.equipos.size();i++){
                 switch (equipos.get(i).getDisponibilidad().toLowerCase()) {
                     case "mañana":
-
+                        equiposMañana.add(equipos.get(i));
+                        break;
+                    case "tarde":
+                        equiposTarde.add(equipos.get(i));
+                        break;
+                    case "noche":
+                        equiposNoche.add(equipos.get(i));
+                        break;
                 }
             }
         }
+        ordenarEquipos(equiposMañana);
+        ordenarEquipos(equiposTarde);
+        ordenarEquipos(equiposNoche);
     }
 }
