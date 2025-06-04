08721bfca9537c9179969b9ed65597eb20dbd0e7
Fyyysh <devmmruiz@gmail.com>
2025-06-04 08:47:24 -0300

    Terminado EJ5

--- Java/Labo2025_Java/src/Sistemas/SistemaProductos.java
+++ Java/Labo2025_Java/src/Sistemas/SistemaProductos.java
@@ -6,5 +6,36 @@ import java.util.ArrayList;
 
 public class SistemaProductos {
     private ArrayList<Producto> productos;
-    
+
+    public SistemaProductos(){
+       this.productos= new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto){
+        this.productos.add(producto);
+    }
+
+    public void eliminarProducto(Producto producto){
+        this.productos.remove(producto);
+    }
+
+    public Producto mayorStock(){
+        Producto mayorStock= this.productos.getFirst();
+        for (Producto p : this.productos){
+            if(p.getStock()>mayorStock.getStock()){
+                mayorStock=p;
+            }
+        }
+        return mayorStock;
+    }
+
+    public Producto menorStock(){
+        Producto menorStock= this.productos.getFirst();
+        for (Producto p : this.productos){
+            if(p.getStock()<menorStock.getStock()){
+                menorStock=p;
+            }
+        }
+        return menorStock;
+    }
 }
